24997e0252a379e02d946edd6a08181519c7e15f
Anand P <anandpamegoze@gmail.com>
2025-12-10 22:58:16 +0530

    update SOCKET_SERVER_URL

--- src/utils/webrtcConfig.js
+++ src/utils/webrtcConfig.js
@@ -32,4 +32,4 @@ export const mediaConstraints = {
 };
 
 // Socket.io server URL
-export const SOCKET_SERVER_URL = 'http://localhost:3001';
+export const SOCKET_SERVER_URL = 'https://webrtc-call-server-806d.onrender.com';
